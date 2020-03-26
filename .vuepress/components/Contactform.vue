7f18e37c1c5d43cececf4c52eb2126826919fa89
inarticulatus <utkarsh1999tripathi@gmail.com>
2020-03-26 15:35:18 +0530

    Forms Updated

--- .vuepress/components/Contactform.vue
+++ .vuepress/components/Contactform.vue
@@ -1,51 +1,11 @@
 <template>
-<div style="padding:10px;">
-  <form
-    name="ask-question"
-    method="post"
-    data-netlify="true"
-    data-netlify-honeypot="bot-field"
-    >
-    <h3> Name </h3>
-    <input type="text" name="Contact" value="" />
-    <br/>
-    <h3> Email ID </h3>
-    <input type="email" name="Contact" value="" />
-    <br/>
-    <h3> How can we help? </h3>
-    <textarea name="comment" form="Contact"></textarea>
-    <br/>
-    <h3> Are you a student/faculty at National Rail and Transportation Institute? </h3>
-    <label v-for="(panelist, index) in panelists" :key="index">
-      <input
-        type="radio"
-        name="panelist"
-        :value="panelist"
-        @input="ev => updatePanelist"
-        :checked="panelist === currentPanelist"
-      />
-      <span>{{ panelist }}</span>
-    </label>
-    <div style="padding-top:20px;">
-    
-    <button>Submit</button>
-    </div>
-  </form>
-  </div>
+
+<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd0n1NQKKME9y8D-ui9lu1e1OSNps_RPgk9EvNqT-MxrQiOKw/viewform?embedded=true" width="760" height="760px" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+
 </template>
+
 <script>
 export default {
-  name: "QAForm",
-  methods: {
-    updatePanelist (ev) {
-      this.currentPanelist = ev.target.value
-    }
-  },
-  data () {
-    return {
-      panelists: ['Yes', 'No'],
-      currentPanelist: 'No'
-    }
-  }
+  name: "Contact"
 }
 </script>
--- contact/Readme.md
+++ contact/Readme.md
@@ -3,18 +3,14 @@ title: Contacts
 ---
 
 
-<h1> Contacts </h1>
-
 <p> 
 
 You can reach out to us for further details or queries.
 
 </p>
 
-<div style="padding-left:50px;">
+<div>
 <p>
- 
 <Contactform/>
-
 </p></div>
 
